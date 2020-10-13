61ef75eda11f5f9a759d523703056f4321dab8ef
Phung82 <pn8778773@gmail.com>
2020-10-13 17:49:00 +0700

    create Odometer

---
+++ app/src/main/java/com/sb/splashactivity/OdometerActivity.java
@@ -0,0 +1,13 @@
+package com.sb.splashactivity;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class OdometerActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_odometer);
+    }
+}
